2d9451439c0c2eadeaf1396c3870584b540377f1
A.K.SAJIN <108419506+sajin-oops@users.noreply.github.com>
2024-10-30 18:33:22 +0530

    Add files via upload

---
+++ database.sql
@@ -0,0 +1,24 @@
+-- drop databases
+
+mysql> DROP DATABASE sajin;
+Query OK, 0 rows affected (0.02 sec)
+
+mysql> DROP DATABASE SYS;
+Query OK, 101 rows affected (0.37 sec)
+
+mysql> DROP DATABASE SAKILA;
+Query OK, 23 rows affected (0.48 sec)
+
+mysql> SHOW DATABASES;
++--------------------+
+| Database           |
++--------------------+
+| information_schema |
+| mysql              |
+| performance_schema |
+| test               |
+| world              |
++--------------------+
+5 rows in set (0.00 sec)
+
+mysql>
